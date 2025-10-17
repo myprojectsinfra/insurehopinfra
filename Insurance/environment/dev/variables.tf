ac88a4f0bd01c660538a04b3b87e129c2edda4c0
jaikumar9760 <jaikumar9760@gmail.com>
2025-10-17 12:40:44 +0530

    Update variables.tf

--- Insurance/environment/dev/variables.tf
+++ Insurance/environment/dev/variables.tf
@@ -1,3 +1,7 @@
+variable "subscription_id" {
+  description = "The id of the subscription."
+  type        = string
+}
 variable "resource_group_name" {
   description = "The name of the resource group."
   type        = string
